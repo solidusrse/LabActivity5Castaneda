da3630306e109f1ea710ad8a33be4943441c8576
josia <josiahzabdiel.castaneda@cit.edu>
2022-12-14 20:48:36 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,14 +1,42 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class LeapYearGUI extends JFrame{
+public class LeapYearGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
 
-    public  LeapYearChecker() {
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkLeapYear();
+            }
+        });
 
+        tfYear.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    checkLeapYear();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -17,5 +45,23 @@ public class LeapYearGUI extends JFrame{
         leap_year.setSize(500, 700);
         leap_year.setDefaultCloseOperation(EXIT_ON_CLOSE);
         leap_year.setVisible(true);
+    }
+
+    public void checkLeapYear() {
+        int year = 0;
+        try {
+            year = Integer.parseInt(tfYear.getText());
+            if(year < 0){
+                JOptionPane.showMessageDialog(null, "Year cannot be negative.", "Warning", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+
+            if (year % 4 == 0) {
+                JOptionPane.showMessageDialog(null, "Leap year");
+            } else
+                JOptionPane.showMessageDialog(null, "Not a leap year");
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Input must be a number.", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
     }
 }
