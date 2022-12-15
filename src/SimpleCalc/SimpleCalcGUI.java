60f74cb5f3b008a20fa7206f03d03ea673b5ce72
josia <josiahzabdiel.castaneda@cit.edu>
2022-12-15 18:35:36 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,14 +1,54 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class SimpleCalcGUI extends JFrame {
+    private String[] operations = {"+", "-", "*", "/"};
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JLabel lblResult;
+    private JTextField tfResult;
+
+    public SimpleCalcGUI() {
+        cbOperations.addItem('+');
+        cbOperations.addItem('-');
+        cbOperations.addItem('*');
+        cbOperations.addItem('/');
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(tfNumber1.getText()!=null && tfNumber2.getText()!=null)
+                    tfResult.setText(String.valueOf(calculate()));
+            }
+        });
+
+        btnCompute.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                    if(tfNumber1.getText()!=null && tfNumber2.getText()!=null)
+                        tfResult.setText(String.valueOf(calculate()));
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         SimpleCalcGUI calc = new SimpleCalcGUI();
@@ -16,5 +56,23 @@ public class SimpleCalcGUI extends JFrame {
         calc.setSize(500, 700);
         calc.setDefaultCloseOperation(EXIT_ON_CLOSE);
         calc.setVisible(true);
+    }
+
+    public long calculate() {
+        long num1 = Long.parseLong(tfNumber1.getText()), num2 = Long.parseLong(tfNumber2.getText());
+
+        if(cbOperations.getSelectedIndex() == 0){
+            return num1 + num2;
+        }
+        if(cbOperations.getSelectedIndex() == 1){
+            return num1 - num2;
+        }
+        if(cbOperations.getSelectedIndex() == 2){
+            return num1 * num2;
+        }
+        if(cbOperations.getSelectedIndex() == 3){
+            return num1 / num2;
+        }
+        return 0;
     }
 }
